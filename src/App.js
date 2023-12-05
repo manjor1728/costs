135aa96378c33817bcca672758cadd264535c226
Narciso Manjor <nlmanjor1728@gmail.com>
2023-12-05 04:37:17 +0000

    menu
    implementação dos links do menu de navegação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/contact'>Contacto</Link>
+        <Link to='/newproject'>Novo Projecto</Link>
+      </ul>
+      <Routes>
+        <Route exact path='/'><Home></Home></Route>
+        <Route path='/company'><Company></Company></Route>
+        <Route path='/contact'><Contact></Contact></Route>
+        <Route path='/newproject'><NewProject></NewProject></Route>
+      </Routes>
+      <p>Rodapé</p>
+    </Router>
   );
 }
 
